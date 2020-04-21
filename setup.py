846f0c5fc355bb25135e0373c012e98ebf080730
Mohit Chugh <mohit.chugh@okta.com>
2020-04-21 12:36:15 -0700

    rel namespace test

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name = "lint",
-    version = "0.0.5",
+    version = "0.0.6",
     author = "mohitchugh",
     author_email="author@example.com",
     description="A small example package",
